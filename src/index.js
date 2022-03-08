a331a26ed06d404cc4aefb6b7a3ef145d34ba397
EvandroFBL <evandrofbl@gmail.com>
2022-03-07 22:08:35 -0300

    middleware para verificar se existe account

--- src/index.js
+++ src/index.js
@@ -8,6 +8,20 @@ app.use(express.json());
 
 const customers = [];
 
+// Middleware to verify if the customer exists
+function VerifyCustomersExistance(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer)
+    return response.status(400).json({error: "Customer not found!"});
+
+  request.customer = customer;
+
+  return next();
+}
+
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
 
@@ -28,13 +42,8 @@ app.post('/account', (request, response) => {
   return response.status(201).send();
 });
 
-app.get('/statement/:cpf', (request, response) => {
-  const { cpf } = request.params;
-
-  const customer = customers.find((customer) => customer.cpf === cpf);
-
-  if (!customer)
-    return response.status(400).json({error: "Customer not found!"})
+app.get('/statement/', VerifyCustomersExistance, (request, response) => {
+  const { customer } = request;
 
   return response.json(customer.statements);
 })
